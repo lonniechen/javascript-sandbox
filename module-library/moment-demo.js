85090a18a1549cc4ffb45591168d611966cd4cc7
lonniechen <lonniechen1024@gmail.com>
2019-05-05 16:00:20 +0800

    update the moment-demo.js

--- module-library/moment-demo.js
+++ module-library/moment-demo.js
@@ -1,7 +1,7 @@
-// install the moment.js module
-// npm install moment.js --save
-var moment = require("moment");
-var timeFormat = "YYYY-MM-DD HH:mm:ss";
+// install the moment module
+// npm install moment
+const moment = require("moment");
+const timeFormat = "YYYY-MM-DD HH:mm:ss";
 console.log(`\x1b[33m%s\x1b[0m`, `-----------------------------`);
 
 // moment() returns the current time
@@ -9,16 +9,16 @@ console.log(moment().format());
 console.log(`\x1b[33m%s\x1b[0m`, `-----------------------------`);
 
 // convert given time to specific format
-var time = "2019-05-01 12:00:00";
+time = "2019-05-01 12:00:00";
 console.log(moment(time, timeFormat).format());
 console.log(moment(time, timeFormat).format(timeFormat));
 console.log(`\x1b[33m%s\x1b[0m`, `-----------------------------`);
 
 // add duration to a time object
 time = moment(time, timeFormat);
-var time1 = moment(time).add(1, "seconds"); //create a new mement object based on time, add one second, pass the reference to time1
-var time2 = moment(time1).add(1, "minutes");
-var time3 = time2.add(-1, "hours"); // subtract one hour from time2, then copy the reference to time3
+time1 = moment(time).add(1, "seconds"); //create a new mement object based on time, add one second, pass the reference to time1
+time2 = moment(time1).add(1, "minutes");
+time3 = time2.add(-1, "hours"); // subtract one hour from time2, then copy the reference to time3
 console.log(time1.format(timeFormat));
 console.log(time2.format(timeFormat));
 console.log(time3.format(timeFormat));
